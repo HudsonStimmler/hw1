e56ac7e50cd5bc995b117ea4bc5a6e0b5c34d720
Hudson Stimmler <hstimmle@usc.edu>
2024-01-28 22:49:36 +0000

    all done with part 1

--- split.cpp
+++ split.cpp
@@ -11,6 +11,7 @@ the function below should be the only one in this file.
 */
 
 #include "split.h"
+#include <cstddef>
 
 /* Add a prototype for a helper function here if you need */
 
@@ -18,6 +19,22 @@ void split(Node*& in, Node*& odds, Node*& evens)
 {
   /* Add code here */
 // WRITE YOUR CODE HERE
+  if(in == NULL){
+    return;
+  }
+  Node* next = in->next;
+  
+  if(in->value % 2 == 0){
+    evens = in;
+    evens->next = nullptr;
+    split(next, odds, evens->next);
+  }
+  else{
+    odds = in;
+    odds->next = nullptr;
+    split(next, odds->next, evens);
+  }
+  in = next;
 }
 
 /* If you needed a helper function, write it here */
